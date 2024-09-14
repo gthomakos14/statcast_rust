c7c627be7bba406d2e438897d6fac615ad4fbec1
gthomakos14 <gthomakos14@gmail.com>
2024-09-14 11:12:42 -0400

    finished up get_box_data function

--- src/api_handler.rs
+++ src/api_handler.rs
@@ -1,9 +1,20 @@
-pub async fn get_game_pks(date: String) -> Vec<String>{
-    let url = format!("https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={date}");
+use polars::prelude::*;
+use std::io::Cursor;
+
+async fn query_url(url: String) -> serde_json::Value{
     let response = reqwest::get(url)
         .await
-        .expect("Can't query schedule API");
-    let schedule_json: serde_json::Map<String, serde_json::Value> = response.json().await.unwrap();
+        .expect("Cannot query provided URL");
+    let json = response
+        .json()
+        .await
+        .expect("Poorly formed JSON. Please investigate.");
+    json
+}
+
+pub async fn get_game_pks(date: String) -> Vec<String>{
+    let url = format!("https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={date}");
+    let schedule_json = query_url(url).await;
     let dates = schedule_json.get("dates");
     // TODO: This is quite complicated. Wrap some of it in a separate function
     match dates {
@@ -36,6 +47,43 @@ pub async fn get_game_pks(date: String) -> Vec<String>{
 }
 
 
-pub async fn get_box_data(game_pk: &str){
+fn format_team_data(team_data: &serde_json::Value) -> DataFrame{
+    let team_name = team_data
+        .get("team")
+        .unwrap()
+        .get("name")
+        .unwrap()
+        .as_str()
+        .unwrap();
+    let batting = team_data
+        .get("teamStats")
+        .unwrap()
+        .get("batting")
+        .unwrap()
+        .to_string();
+    let cursor = Cursor::new(batting.as_bytes());
+    let df = JsonReader::new(cursor)
+        .finish()
+        .unwrap()
+        .lazy()
+        .with_column(lit(team_name).alias("teamName"))
+        .collect()
+        .unwrap();
+    df
+}
+
+
+pub async fn get_box_data(game_pk: &str) -> DataFrame{
     let url = format!("https://statsapi.mlb.com/api/v1/game/{game_pk}/boxscore");
+    let response = query_url(url).await;
+    let teams_json = response.get("teams").unwrap();
+    let box_data = vec![
+        format_team_data(teams_json.get("away").unwrap()).lazy(),
+        format_team_data(teams_json.get("home").unwrap()).lazy()];
+    let lf = concat(box_data,
+         UnionArgs::default()).expect("Cannot concat home and away box data")
+         .with_column(lit(game_pk).alias("gamePk"));
+    let df = lf.collect().unwrap();
+
+    df
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@ mod api_handler;
 
 #[tokio::main]
 async fn main() {
-    let date = "2023-12-30".to_owned();
+    // let date = "2023-06-30".to_owned();
+    // print_game_pks(date);
+    let yankees_pk = "717550";
+    let mut df_box = api_handler::get_box_data(yankees_pk)
+        .await;
+    println!("{}", &df_box);
+    let mut file = std::fs::File::create("yankees_wedding_box.parquet").unwrap();
+    polars::prelude::ParquetWriter::new(&mut file).finish(&mut df_box).unwrap();
+}
+
+
+async fn print_game_pks(date: String){
     let game_pks = api_handler::get_game_pks(date).await;
     let games_length = game_pks.len();
     println!("{games_length}");
